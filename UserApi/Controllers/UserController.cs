25ed3bd68e39b588a986ce4808f19b2304feceef
sajal.shrestha <sajal.shrestha@info.com.np>
2023-12-19 17:28:09 +0545

    UserModel implementation

---
+++ UserApi/Controllers/UserController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UserApi.Models;
+using UserApi.Services;
+
+namespace UserApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService userService;
+        public UserController(IUserService _userService)
+        {
+            userService = _userService;
+        }
+        [HttpGet]
+        public IEnumerable<User> UserList()
+        {
+            var userList = userService.GetUserList();
+            return userList;
+        }
+        [HttpGet("{id}")]
+        public User GetUserById(int Id)
+        {
+            return userService.GetUserById(Id);
+        }
+        [HttpPost]
+        public User AddUser(User user)
+        {
+            return userService.AddUser(user);
+        }
+        [HttpPut]
+        public User UpdateUser(User user)
+        {
+            return userService.UpdateUser(user);
+        }
+        [HttpDelete("{id}")]
+        public bool DeleteUser(int id)
+        {
+            return userService.DeleteUser(id);
+        }
+    }
+}
--- UserApi/Program.cs
+++ UserApi/Program.cs
@@ -1,7 +1,12 @@
+using UserApi.Data;
+using UserApi.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddDbContext<DbContextClass>();    
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
